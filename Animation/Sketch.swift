b73026277368fa50ff4285844fd4fddb8e0c2c65
Diaz Octavio <odiaz@lcs-mba3082.lcs.on.ca>
2017-10-26 11:39:58 -0400

    got the circles to move the right direction!!! : )

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -7,8 +7,8 @@ class Sketch : NSObject {
     let canvas : Canvas
     
     // Position of circle
-    var x : Int
-    
+    //change in position
+    var offset : Int
     // This function runs once
     override init() {
         
@@ -16,18 +16,46 @@ class Sketch : NSObject {
         canvas = Canvas(width: 500, height: 500)
         
         // Set starting position
-        x = 250
-        
+        offset = 250
+        // set dx (change in x) value
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
+        //clean up draw whit rectangle over whole canvas
+        
+        
+        
         
         // Change position
-        x += 1
+        offset += 1
         
-        // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        //black circle going right (+)
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(centreX: offset, centreY: 83, width: 50, height: 50)
+        
+        //blue circle going left (-)
+        
+        canvas.fillColor = Color(hue: 0, saturation: 80, brightness: 0, alpha: 100)
+        canvas.drawEllipse(centreX: 500 - offset, centreY: 166, width: 50, height: 50)
+        
+        
+        //green circle going right (+)
+        
+        canvas.fillColor = Color(hue: 0, saturation: 80, brightness: 0, alpha: 100)
+        canvas.drawEllipse(centreX: offset, centreY: 249, width: 50, height: 50)
+        
+        //orange circle going left (-)
+        
+        canvas.fillColor = Color(hue: 0 ,saturation: 80, brightness: 0, alpha: 100)
+        canvas.drawEllipse(centreX: 500 - offset, centreY: 332, width: 50, height: 50)
+        
+        //purple circle going right (+)
+        
+        canvas.fillColor = Color(hue: 0 ,saturation: 80, brightness: 0, alpha: 100)
+        canvas.drawEllipse(centreX: offset, centreY: 415, width: 50, height: 50)
+        
+        
         
     }
     
