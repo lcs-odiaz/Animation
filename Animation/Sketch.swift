526af123622fd4bce61ea779e2db55ec7ee80f2f
Diaz Octavio <odiaz@lcs-mba3082.lcs.on.ca>
2017-10-28 11:48:34 -0400

    FINIHSED. All circles going right directions with colour.

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,7 +8,7 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
-    
+    var y : Int
     // This function runs once
     override init() {
         
@@ -17,18 +17,35 @@ class Sketch : NSObject {
         
         // Set starting position
         x = 250
-        
+        y = 250
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
+        canvas.drawShapesWithBorders = false
+        
         // Change position
         x += 1
+        y += 1
         
-        // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
         
+        //upright red
+        canvas.fillColor = Color(hue: 360, saturation: 100, brightness: 90, alpha: 100)
+        canvas.drawEllipse(centreX: x, centreY: y, width: 50, height: 50)
+        
+        //downleft yellow
+        canvas.fillColor = Color(hue: 56, saturation: 100, brightness: 90, alpha: 100)
+        canvas.drawEllipse(centreX: 500-x, centreY: 500-y, width: 50, height: 50)
+        
+        //upleft green
+        canvas.fillColor = Color(hue: 123, saturation: 100, brightness: 90, alpha: 100)
+        canvas.drawEllipse(centreX: 500-x, centreY: y, width: 50, height: 50)
+        
+        //downright blue
+        canvas.fillColor = Color(hue: 240, saturation: 100, brightness: 90, alpha: 100)
+        canvas.drawEllipse(centreX: x, centreY: 500-y, width: 50, height: 50)
+
     }
     
 }
